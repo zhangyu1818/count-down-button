0fc9e04a1d4e685b7ad03800518f0ad4c9486618
zhangyu1818 <723156735@qq.com>
2020-08-12 22:56:00 +0800

    Update README.md

--- .umirc.ts
+++ .umirc.ts
@@ -6,5 +6,8 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  publicPath: 'https://zhangyu1818.github.io/countdown-button/',
+  base: '/countdown-button/',
+  exportStatic: {},
   // more config: https://d.umijs.org/config
 });
